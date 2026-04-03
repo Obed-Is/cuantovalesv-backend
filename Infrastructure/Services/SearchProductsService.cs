e34d482bfa006c4883d44e4322ec52a6290def45
Obed-Is <obedaquino71@gmail.com>
2026-04-02 20:41:08 -0600

    refactor: ejecucion de scrapers en paralelo

--- Infrastructure/Services/SearchProductsService.cs
+++ Infrastructure/Services/SearchProductsService.cs
@@ -20,13 +20,12 @@ namespace Infrastructure.Services
         {
             if (string.IsNullOrWhiteSpace(searchTerm.Trim()) || searchTerm.Length >= 40) throw new AppExceptionStatusCode(400, "Los datos ingresados son invalidos");
 
-            var products = new List<ProductDto>();
-
-            foreach(var scraper in _scraperServices)
-            {
-                var product = await scraper.SearchProductsAsync(searchTerm);
-                products.AddRange(product);
-            }
+            //ejecuta todos los scrapers en paralelo(los crea tipo Task)
+            var tasks = _scraperServices.Select(async sc => await sc.SearchProductsAsync(searchTerm)).ToList();
+            // espera que todos los scrapers finalizen
+            List<ProductDto>[] results = await Task.WhenAll(tasks);
+            // convierte los restultados a 1 nivel ejm: [1, 2, [3, 4], [5, 6,], 7] = [1, 2, 3, 4, 5, 6, 7] 
+            var products = results.SelectMany(p => p).ToList();
 
             return products;
         }
