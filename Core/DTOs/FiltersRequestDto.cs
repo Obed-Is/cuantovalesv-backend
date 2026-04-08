3d7cae1e44a985142019bd9d42fca8c84bfa4ea6
Obed-Is <obedaquino71@gmail.com>
2026-04-07 23:46:35 -0600

    feat(DTOs): se agrega nuevo DTOs para filtros en request

---
+++ Core/DTOs/FiltersRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public class FiltersRequestDto
+    {
+        public List<string> Filters { get; set; } = new List<string>();
+    }
+}
